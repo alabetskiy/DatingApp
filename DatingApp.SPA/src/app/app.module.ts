5adf3d3cebe0c9eb30a8a7b6c6c3f8006bc289c6
Artem Labetskiy <alabetskiy@frontlineed.com>
2018-01-05 19:17:14 -0500

    Added Bootstrap and font-awesome. Added CROS. Frontend is hooked with backend

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
+       
   ],
   providers: [],
   bootstrap: [AppComponent]
