13cab33e9d0e4fca4f48e5176ab951d58e8aa48e
labetskiy <labetskiy@gmail.com>
2018-01-15 13:06:39 -0500

    Added editForm in MemberEdit component

--- DatingApp.SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp.SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,9 @@
+import { AlertifyService } from './../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from './../../_models/User';
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,12 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberEditComponent implements OnInit {
 user:User;
-  constructor(private route:ActivatedRoute) { } //we need to inject ActivatedRoute because it will contain data, which we are fetching inside our route resolver (!)
+@ViewChild('editForm') editForm: NgForm;
+
+  constructor(private route:ActivatedRoute, private alertify:AlertifyService) { } //we need to inject ActivatedRoute because it will contain data, which we are fetching inside our route resolver (!)
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     })
+  }
+
+  updateUser(){
+this.alertify.success('Profile updated sucessfully');
+this.editForm.reset(this.user); //we are reseting the staet of our Form
   }
 
 }
