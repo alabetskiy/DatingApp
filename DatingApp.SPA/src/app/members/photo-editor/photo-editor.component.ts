aa27c10e8bfbe70d2f6ad516968be39d54426c64
labetskiy <labetskiy@gmail.com>
2018-01-16 16:21:23 -0500

    Initialize Uploader in photo-edit component

--- DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from '../../_models/Photo';
 import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,14 +11,42 @@ import { FileUploader } from 'ng2-file-upload';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[]
-  uploader: FileUploader = new FileUploader({});  //copied from https://valor-software.com/ng2-file-upload/
+  uploader: FileUploader  //copied from https://valor-software.com/ng2-file-upload/
   hasBaseDropZoneOver: boolean = false;           //copied from https://valor-software.com/ng2-file-upload/
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
   }
-  public fileOverBase(e:any):void { //copied from https://valor-software.com/ng2-file-upload/
+  public fileOverBase(e: any): void { //copied from https://valor-software.com/ng2-file-upload/
     this.hasBaseDropZoneOver = e;   //copied from https://valor-software.com/ng2-file-upload/
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onSuccessItem = (item, response, stauts, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        }
+        this.photos.push(photo);
+      };
+    }
   }
 }
